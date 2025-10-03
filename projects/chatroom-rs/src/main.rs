ccdcfef7b7e5cdc63556879a69d056400e95373f
SS56Bo <sohamsarkar345@gmail.com>
2025-10-04 01:16:55 +0530

    Chatapp done

--- projects/chatroom-rs/src/main.rs
+++ projects/chatroom-rs/src/main.rs
@@ -1,11 +1,55 @@
+use rocket::{
+    fs::{FileServer, relative},
+    response::stream::{Event, EventStream},
+    tokio::select,
+    tokio::sync::{broadcast::{error::RecvError, Sender}, broadcast},
+    Shutdown, State,
+};
+use serde::{Serialize, Deserialize};
+use rocket::form::Form;
+
 #[macro_use] extern crate rocket;
 
-#[get("/greet")]
-fn greeting() -> &'static str {
-    "hello world!"
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..30))]
+    pub username: String,
+    pub message: String,
+}
+
+#[post("/message", data = "<form>")]
+fn mailer(form: Form<Message>, queue: &State<Sender<Message>>) {
+    let _ = queue.send(form.into_inner());
+}
+
+#[get("/events")]
+fn get_messages(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut receiver = queue.subscribe();
+
+    EventStream! {
+        loop {
+            let message = select! {
+                msg = receiver.recv() => match msg {
+                    Ok(m) => m,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&message);
+        }
+    }
 }
 
 #[launch]
-fn rocket()->_ {
-    rocket::build().mount("/", routes![greeting])
+fn rocket() -> _ {
+    let (tx, _rx) = broadcast::channel::<Message>(1024);
+
+    rocket::build()
+        .manage(tx)
+        .mount("/", routes![mailer, get_messages])
+        .mount("/", FileServer::from(relative!("frontend")))
 }
